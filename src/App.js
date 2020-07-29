4bba1863508110336cf650da385c8bf52827111c
Sone <sthaya@gmail.com>
2020-07-29 11:01:14 -0700

    added about to readme file and about to game. added presets and ability to move next one generation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Buttons from './components/Buttons'
 import Rules from './components/Rules'
 
 
+
 class App extends React.Component {
 	constructor() {
 		super();
@@ -42,6 +43,101 @@ class App extends React.Component {
 		});
 	}
 
+	dieHard = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		
+					gridCopy[17][16] = true;
+					gridCopy[18][10] = true;
+					gridCopy[18][11] = true;
+					gridCopy[19][11] = true;
+					gridCopy[19][15] = true;
+					gridCopy[19][16] = true;
+					gridCopy[19][17] = true;
+		
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+	gliderGun = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		
+					gridCopy[5][1] = true;
+					gridCopy[5][2] = true;
+					gridCopy[6][1] = true;
+					gridCopy[6][1] = true;
+					gridCopy[3][13] = true;
+					gridCopy[3][14] = true;
+					gridCopy[4][12] = true;
+					gridCopy[4][16] = true;
+					gridCopy[5][11] = true;
+					gridCopy[5][17] = true;
+					gridCopy[6][11] = true;
+					gridCopy[6][15] = true;
+					gridCopy[6][17] = true;
+					gridCopy[6][18] = true;
+					gridCopy[7][11] = true;
+					gridCopy[7][17] = true;
+					gridCopy[8][12] = true;
+					gridCopy[8][16] = true;
+					gridCopy[9][13] = true;
+					gridCopy[9][14] = true;
+					gridCopy[1][25] = true;
+					gridCopy[2][23] = true;
+					gridCopy[2][25] = true;
+					gridCopy[3][21] = true;
+					gridCopy[3][22] = true;
+					gridCopy[4][21] = true;
+					gridCopy[4][22] = true;
+					gridCopy[5][21] = true;
+					gridCopy[5][22] = true;
+					gridCopy[6][23] = true;
+					gridCopy[6][25] = true;
+					gridCopy[7][25] = true;
+					gridCopy[3][35] = true;
+					gridCopy[3][36] = true;
+					gridCopy[4][35] = true;
+					gridCopy[4][36] = true;
+		
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+	pulsar = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		
+					gridCopy[2][4] = true;
+					gridCopy[2][5] = true;
+					gridCopy[2][6] = true;
+					gridCopy[2][10] = true;
+					gridCopy[2][11] = true;
+					gridCopy[2][12] = true;
+					gridCopy[4][2] = true;
+					gridCopy[4][7] = true;
+					gridCopy[4][9] = true;
+					gridCopy[4][14] = true;
+					gridCopy[5][2] = true;
+					gridCopy[5][7] = true;
+					gridCopy[5][9] = true;
+					gridCopy[5][14] = true;
+					gridCopy[6][2] = true;
+					gridCopy[6][7] = true;
+					gridCopy[6][9] = true;
+					gridCopy[6][14] = true;
+					gridCopy[7][4] = true;
+					gridCopy[7][5] = true;
+					gridCopy[7][6] = true;
+					gridCopy[7][10] = true;
+					gridCopy[7][11] = true;
+					gridCopy[7][12] = true;
+		
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+
 	playButton = () => {
 		clearInterval(this.intervalId);
 		this.intervalId = setInterval(this.play, this.speed);
@@ -52,7 +148,12 @@ class App extends React.Component {
 	}
 
 	slow = () => {
-		this.speed = 1000;
+		this.speed = 1200;
+		this.playButton();
+	}
+
+	medium = () => {
+		this.speed = 700;
 		this.playButton();
 	}
 
@@ -72,8 +173,8 @@ class App extends React.Component {
 	gridSize = (size) => {
 		switch (size) {
 			case "1":
-				this.cols = 20;
-				this.rows = 20;
+				this.cols = 25;
+				this.rows = 25;
 			break;
 			case "2":
 				this.cols = 40;
@@ -84,6 +185,34 @@ class App extends React.Component {
 				this.rows = 50;
 		}
 		this.clear();
+
+	}
+
+	nextStep = () => {
+		let g = this.state.gridFull;
+		let g2 = arrayClone(this.state.gridFull);
+
+		for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+				if (!g[i][j] && count === 3) g2[i][j] = true;
+				
+				if (g[i][j] === false && g2[i][j] === false) this.pauseButton()
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
 
 	}
 
@@ -103,7 +232,7 @@ class App extends React.Component {
 		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
 		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
 		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
-		    if (!g[i][j] && count === 3) g2[i][j] = true;
+				if (!g[i][j] && count === 3) g2[i][j] = true;
 		  }
 		}
 		this.setState({
@@ -126,10 +255,15 @@ class App extends React.Component {
 					playButton={this.playButton}
 					pauseButton={this.pauseButton}
 					slow={this.slow}
+					medium={this.medium}
 					fast={this.fast}
 					clear={this.clear}
 					seed={this.seed}
+					dieHard={this.dieHard}
+					gliderGun={this.gliderGun}
+					pulsar={this.pulsar}
 					gridSize={this.gridSize}
+					nextStep={this.nextStep}
 				/>
 				<Grid
 					gridFull={this.state.gridFull}
@@ -146,7 +280,10 @@ class App extends React.Component {
 				}}>
 					<Rules />
 				</div>
+
 				
+				<h4>About</h4>
+				<p>The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
 			</div>
 		);
 	}
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -12,30 +12,61 @@ class Buttons extends React.Component {
 			<div className="center">
 				
 					<Button className="btn btn-primary" onClick={this.props.playButton}>
-					Start
+						Start
 					</Button>
 					
 					<Button className="btn btn-default" onClick={this.props.pauseButton}>
-					  Stop
+						Stop
+					</Button>
+					<Button className="btn btn-default" onClick={this.props.nextStep}>
+						Next
 					</Button>
 					<Button className="btn btn-default" onClick={this.props.clear}>
-					  Clear
+						Clear
 					</Button>
-					<Button className="btn btn-default" onClick={this.props.slow}>
-					  Slow
-					</Button>
-					<Button className="btn btn-default" onClick={this.props.fast}>
-					  Fast
-					</Button>
-					<Button className="btn btn-default" onClick={this.props.seed}>
-					  Random
-					</Button>
+				
+					<DropdownButton
+						title="Speed"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<Button className="btn btn-default" onClick={this.props.slow}>
+							Slow
+						</Button>
+						<Button className="btn btn-default" onClick={this.props.medium}>
+							Medium
+						</Button>
+					
+						<Button className="btn btn-default" onClick={this.props.fast}>
+							Fast
+						</Button>
+					</DropdownButton>
+					
+					<DropdownButton
+						title="Presets"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<Button className="btn btn-default" onClick={this.props.seed}>
+							Random
+						</Button>
+						<Button className="btn btn-default" onClick={this.props.dieHard}>
+							Diehard
+						</Button>
+						<Button className="btn btn-default" onClick={this.props.gliderGun}>
+							Glider Gun
+						</Button>
+						<Button className="btn btn-default" onClick={this.props.pulsar}>
+						Pulsar
+						</Button>
+					</DropdownButton>
+
 					<DropdownButton
 						title="Grid Size"
 						id="size-menu"
 						onSelect={this.handleSelect}
 					>
-						<Dropdown.Item eventKey="1">20x20</Dropdown.Item>
+						<Dropdown.Item eventKey="1">25x25</Dropdown.Item>
 						<Dropdown.Item eventKey="2">40x40</Dropdown.Item>
 						<Dropdown.Item eventKey="3">50x50</Dropdown.Item>
 					</DropdownButton>
